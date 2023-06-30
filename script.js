b8aeafbeb7566257cd0ba35d0be53b13ae60e586
Daymond Holmes <daymondholmes@gmail.com>
2023-06-30 04:51:14 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let bio  = document.getElementById("introduction")
+
+let firstName = "Daymond";
+let lastName = "Holmes";
+let yearOfStudy = 1 + " year";
+let goal = "Continue to get my associates in programming and it"
+
+bio.innerHTML = `My name is ${firstName} ${lastName}. I have been at SCC for ${yearOfStudy} and plan to ${goal} after completing my courses.`;
+
+
